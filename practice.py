efd7dda610437e34d2c4f7fdce94376960b844bd
Pavithra0405 <pavithravs45@gmail.com>
2026-05-17 21:38:44 +0530

    type conversion

--- practice.py
+++ practice.py
@@ -1,14 +1,17 @@
 print('Hi this is my first python program')
 
 # input and output
-print('Hello,What is ur name?')
-name = input()
-print('Hi,',name)
+# print('Hello,What is ur name?')
+# name = input()
+# print('Hi,',name)
 
 # operators
-num1 = 10
-num2 = 5
-num3 = 12
+print('Enter num1')
+num1 = int(input())
+print('Enter num2')
+num2 = int(input())
+print('Enter num3')
+num3 = int(input())
 print(num1 + num2)
 print(num1 - num2)
 print(num1 * num2)
@@ -16,3 +19,7 @@ print(num3 / num2)
 print(num1 ** num2)
 print(num1 % num2)
 print(num3 // num2)
+print(type(num1))
+print(type(num2))
+print(type(num3))
+
