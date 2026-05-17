71a71e3dad02774eb3c6bb13903e724601f1919c
Pavithra0405 <pavithravs45@gmail.com>
2026-05-17 21:33:06 +0530

    operators

--- practice.py
+++ practice.py
@@ -1,5 +1,18 @@
 print('Hi this is my first python program')
 
+# input and output
 print('Hello,What is ur name?')
 name = input()
 print('Hi,',name)
+
+# operators
+num1 = 10
+num2 = 5
+num3 = 12
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num3 / num2)
+print(num1 ** num2)
+print(num1 % num2)
+print(num3 // num2)
